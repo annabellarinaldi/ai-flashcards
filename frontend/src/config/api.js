bc7c37674f1e57c963c52294d48056ae2edb7ac6
Annabella Rinaldi <arinaldi31@gatech.edu>
2025-08-06 17:00:31 -0400

    fixed env file

--- frontend/src/config/api.js
+++ frontend/src/config/api.js
@@ -1,3 +1,2 @@
-const API_URL = process.env.REACT_APP_API_URL || 'https://ai-flashcards-tivc.onrender.com'
-
+const API_URL = process.env.API_URL || 'https://ai-flashcards-tivc.onrender.com'
 export default API_URL
